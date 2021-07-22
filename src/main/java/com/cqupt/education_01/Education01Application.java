756a800debabff7adddee9f3864ad56455fe3144
kangxuan <1350291587@qq.com>
2021-07-22 08:45:28 +0800

    项目配置

---
+++ src/main/java/com/cqupt/education_01/Education01Application.java
@@ -0,0 +1,13 @@
+package com.cqupt.education_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Education01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Education01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/cqupt/education_01/Education01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cqupt.education_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Education01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
